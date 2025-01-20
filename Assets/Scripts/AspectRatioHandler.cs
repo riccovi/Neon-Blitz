69aba2c0080da2d903eceae52e8cc3c2ab8531cb
riccovi <visserricco@gmail.com>
2025-01-20 12:22:25 +0200

    Level design

--- Assets/Scripts/AspectRatioHandler.cs
+++ Assets/Scripts/AspectRatioHandler.cs
@@ -6,31 +6,31 @@ public class AspectRatioHandler : MonoBehaviour
 {
     private void Start()
     {
-        Camera.main.aspect = 4f / 5f; // Set to 4:5 ratio
-        // float targetApect = 4.0f / 5.0f; // 4:5 aspect ratio
-        // float windowAspect = float(Screen.width) / float(Screen.height);
-        // float scaleHeight = windowAspect / targetAspect;
+        //Camera.main.aspect = 4f / 5f; // Set to 4:5 ratio
+        float targetAspect = 4.0f / 5.0f; // 4:5 aspect ratio
+        float windowAspect = (float)Screen.width / (float)Screen.height;
+        float scaleHeight = windowAspect / targetAspect;
 
-        // Camera camera = Camera.main;
+        Camera camera = Camera.main;
 
-        // if (scaleHeight < 1.0f)
-        // {
-        //     Rect rect = camera.rect;
-        //     rect.width = 1.0f;
-        //     rect.height = scaleHeight;
-        //     rect.x = 0;
-        //     rect.y = (1.0f - scaleHeight) / 2.0f;
-        //     camera.rect = rect;  
-        // }
-        // else
-        // {
-        //     float scaleWidth = 1.0f / scaleHeight;
-        //     Rect rect = camera.rect;
-        //     rect.width = scaleWidth;
-        //     rect.height = 1.0f;
-        //     rect.x = (1.0f - scaleWidth) / 2.0f;
-        //     rect.y = 0;
-        //     camera.rect = rect;
-        // }
+        if (scaleHeight < 1.0f)
+        {
+            Rect rect = camera.rect;
+            rect.width = 1.0f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+            camera.rect = rect;  
+        }
+        else
+        {
+            float scaleWidth = 1.0f / scaleHeight;
+            Rect rect = camera.rect;
+            rect.width = scaleWidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+            rect.y = 0;
+            camera.rect = rect;
+        }
     }
 }
